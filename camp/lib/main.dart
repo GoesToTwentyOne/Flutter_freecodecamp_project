e2d2ab9047bf97bf6345607b802f6b46e530043d
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-09-30 03:01:35 +0600

    reinforce practice

--- camp/lib/main.dart
+++ camp/lib/main.dart
@@ -1,37 +1,58 @@
 import 'package:flutter/material.dart';
 main(){
-  runApp(MaterialApp(
-    home:Homepage(),
+  runApp( MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
     theme: ThemeData(
-      primarySwatch: Colors.purple
+      primarySwatch: Colors.green,
     ),
-  ));
+
+  ),);
 }
-class Homepage extends StatelessWidget {
-  const Homepage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Great"),
-
-      ),
-      body: Center(
-        child: Container(
-          height: 100,
-          width: 100,
-          padding: const EdgeInsets.all(8),
-          alignment: Alignment.center,
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(
-              color: Colors.purple,
-            shape: BoxShape.circle,
+        title:const  Text(
+            "Googles Golang",
+          style: TextStyle(
+            color: Colors.white,
           ),
-          child: Text("Flutter expert"),
+
         ),
       ),
+      body: Center(child:Container(
+        height: 120,
+          width: 120,
+        child: const Center(child:Text(
+          "Hi from googler",
+          style: TextStyle(
+            color: Colors.white,
+
+          ),
+        ),),
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(colors:  [
+            Colors.green,
+            Colors.red,
+
+          ],),
+          color: Colors.purple,
+          shape: BoxShape.circle,
+         border: Border.all(color: Colors.black),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.green,
+              blurRadius: 12,
+            )
+          ]
+
+        )
+      ),),
+
     );
   }
 }
-
