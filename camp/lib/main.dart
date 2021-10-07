b37e7358229ce0a923c8d415ef0df3ca6765b40c
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-10-08 01:21:14 +0600

    flutter basic

--- camp/lib/main.dart
+++ camp/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 main(){
   runApp( MaterialApp(
-    home: HomePage(),
+    // home: HomePage(),
+    home: Colrow(),
+
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
       primarySwatch: Colors.green,
@@ -9,50 +11,83 @@ main(){
 
   ),);
 }
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title:const  Text(
+//             "Googles Golang",
+//           style: TextStyle(
+//             color: Colors.white,
+//           ),
+//
+//         ),
+//       ),
+//       body: Center(child:Container(
+//         height: 120,
+//           width: 120,
+//         child: const Center(child:Text(
+//           "Hi from googler",
+//           style: TextStyle(
+//             color: Colors.white,
+//
+//           ),
+//         ),),
+//         decoration: BoxDecoration(
+//           gradient: const LinearGradient(colors:  [
+//             Colors.green,
+//             Colors.red,
+//
+//           ],),
+//           color: Colors.purple,
+//           shape: BoxShape.circle,
+//          border: Border.all(color: Colors.black),
+//           boxShadow: const [
+//             BoxShadow(
+//               color: Colors.green,
+//               blurRadius: 12,
+//             )
+//           ]
+//
+//         )
+//       ),),
+//
+//     );
+//   }
+// }
+class Colrow extends StatelessWidget {
+  const Colrow({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:const  Text(
-            "Googles Golang",
-          style: TextStyle(
-            color: Colors.white,
-          ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: 100,
+          width: 100,
+          color: Colors.red,
 
         ),
-      ),
-      body: Center(child:Container(
-        height: 120,
-          width: 120,
-        child: const Center(child:Text(
-          "Hi from googler",
-          style: TextStyle(
-            color: Colors.white,
+        Container(
+          height: 100,
+          width: 100,
+          color: Colors.green,
 
-          ),
-        ),),
-        decoration: BoxDecoration(
-          gradient: const LinearGradient(colors:  [
-            Colors.green,
-            Colors.red,
+        ),
+        Container(
+          height: 100,
+          width: 100,
+          color: Colors.pink,
 
-          ],),
-          color: Colors.purple,
-          shape: BoxShape.circle,
-         border: Border.all(color: Colors.black),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.green,
-              blurRadius: 12,
-            )
-          ]
-
-        )
-      ),),
+        ),
+      ],
 
     );
   }
 }
+
---
+++ onebasicinaugration/android/app/src/main/kotlin/com/example/onebasicinaugration/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.onebasicinaugration
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
