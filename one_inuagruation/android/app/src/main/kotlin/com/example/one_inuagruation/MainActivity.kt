5cae33e7d918138535432212fd29d3c3b5dbf310
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-10-08 01:54:39 +0600

    inuagruation

---
+++ one_inuagruation/android/app/src/main/kotlin/com/example/one_inuagruation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.one_inuagruation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ one_inuagruation/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+void main()=> runApp(MyApp());
+class MyApp extends StatelessWidget{
+  const MyApp({Key ? key}) :super(key: key);
+  Widget build(BuildContext context){
+    return MaterialApp(
+     home: Container(
+       child: Text("Hello"),
+     ),
+    );
+  }
+}
